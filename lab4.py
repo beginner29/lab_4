12091c077f35c67d5c4f3cbec701cfcc10e95a43
beginner29 <miro2029@gmail.com>
2019-06-19 12:57:16 +0900

    Jun 19 final commit in class

--- lab4.py
+++ lab4.py
@@ -10,10 +10,24 @@
 ##while count<l:
 ##    if x[count] == "a" or x[count] == "e" or x[count] == "i" or x[count] == "o" or x[count] == "u":
 ##        counter = counter+1
+##        print("counter:",counter)
+##        
 ##    count = count+1
+##    print(count)
 ##
 ##print("Number of vowels is",counter)
-##
+
 
 
 #2
+
+lol = [['a','b','c'],['d','e','f'],['g','h','i']]
+
+#2-(a)
+lala = "."
+for i in lol:
+    for j in i:
+        lala+=".."
+        j=lala+j
+        print(j)
+
