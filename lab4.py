2c1845e6d4c42343ad537e5b9cd46d055be66cc9
Selene Roh <sroh@Selenes-MacBook-Pro.local>
2019-06-19 10:50:13 +0900

    adding existing lines of code to repo

---
+++ lab4.py
@@ -0,0 +1,19 @@
+
+
+###1
+## 
+##x = input("Enter a string: ")
+##l = len(x)
+##count = 0
+##counter = 0
+##
+##while count<l:
+##    if x[count] == "a" or x[count] == "e" or x[count] == "i" or x[count] == "o" or x[count] == "u":
+##        counter = counter+1
+##    count = count+1
+##
+##print("Number of vowels is",counter)
+##
+
+
+#2
